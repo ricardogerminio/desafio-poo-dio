f35521e4cd51ff816d977c322a7df25d83a328cc
Ricardo Germinio <germinioricardo@gmail.com>
2023-02-26 06:03:07 -0300

    Atualização do DateFormatter

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -1,9 +1,11 @@
 package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Mentoria extends Conteudo {
     private LocalDate data;
+    DateTimeFormatter dateTimeFormatterBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     @Override
     public double calcularXP() {
@@ -23,7 +25,7 @@ public class Mentoria extends Conteudo {
         return "Mentoria{" +
                 "titulo='" + getTitulo() + '\'' +
                 ", descricao='" + getDescricao() + '\'' +
-                ", data=" + data +
+                ", data=" + data.format(dateTimeFormatterBR) +
                 '}';
     }
 }
